17c70fe5446ab563b02cf252b8d37ae026c4ba1d
Wan Muzaffar <wanmuz86@gmail.com>
2025-12-05 14:32:10 +0800

    setup hostapp

--- host-app/src/main.tsx
+++ host-app/src/main.tsx
@@ -3,8 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { registerMicroApps, start } from 'qiankun'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+// 1) Code to process the registration of microapp in the host app
+registerMicroApps([
+  {
+    name:'react-app',
+    entry:'//localhost:4173/micro-app/',
+    container:'#micro-app-container',
+    activeRule:'/app' // everything that starts with app will go to microApp
+  }
+])
+
+start()
